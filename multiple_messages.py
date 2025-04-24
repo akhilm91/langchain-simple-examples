8ad803327b2b298a49a363356ca8badc6eaacd6d
Akhil Mohan <akhilm1009@gmail.com>
2025-04-24 20:53:00 +0200

    fix for N813

--- multiple_messages.py
+++ multiple_messages.py
@@ -1,17 +1,17 @@
-from langchain.chat_models import ChatOpenAI as chat_open_ai
-from langchain.schema import HumanMessage as human_message
+from langchain.chat_models import ChatOpenAI
+from langchain.schema import HumanMessage
 from dotenv import load_dotenv
 import os
 
 load_dotenv()
 
 api_key = os.getenv("OPENAI_API_KEY")
-chat_model = chat_open_ai(openai_api_key=api_key)
+chat_model = ChatOpenAI(openai_api_key=api_key)
 
 messages = [
-    human_message(content="from now on 1 + 1 = 3, use this in your replies"),
-    human_message(content="what is 1 + 1"),
-    human_message(content="what is 1 + 1 + 1"),
+    HumanMessage(content="from now on 1 + 1 = 3, use this in your replies"),
+    HumanMessage(content="what is 1 + 1"),
+    HumanMessage(content="what is 1 + 1 + 1"),
 ]
 
 result = chat_model.predict_messages(messages)
